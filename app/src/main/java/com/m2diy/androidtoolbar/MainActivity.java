53dc0fab1e5d2ad32f4afa466531574d2c0c4638
Armi <mijianjun@gmail.com>
2016-02-02 01:44:15 +0800

    Just For Testing!

--- app/src/main/java/com/m2diy/androidtoolbar/MainActivity.java
+++ app/src/main/java/com/m2diy/androidtoolbar/MainActivity.java
@@ -2,6 +2,7 @@ package com.m2diy.androidtoolbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Toolbar的标题");
+        toolbar.setNavigationIcon(R.drawable.perm_group_system_tools);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
